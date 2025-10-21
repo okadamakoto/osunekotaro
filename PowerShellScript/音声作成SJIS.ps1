ea4cce760b056ed0c3948cae4ce1c492006b9be3
okadamakoto <osunekotaro.sippo.in.wind@gmail.com>
2025-10-21 17:37:19 +0900

    Add files via upload

---
+++ PowerShellScript/音声作成SJIS.ps1
@@ -0,0 +1,20 @@
+﻿Add-Type -AssemblyName System.Speech
+
+$speak = New-Object System.Speech.Synthesis.SpeechSynthesizer
+$speak.SelectVoice("Microsoft Haruka Desktop")
+$speak.Rate = -2
+
+# 出力先ファイル
+$outputPath = "C:\okada\text\基本情報技術者試験.wav"
+$textPath = "C:\okada\text\基本情報技術者試験.txt"
+
+# SJISでテキストを読み込む
+$encoding = [System.Text.Encoding]::GetEncoding("shift_jis")
+$reader = New-Object System.IO.StreamReader($textPath, $encoding)
+$text = $reader.ReadToEnd()
+$reader.Close()
+
+# 音声ファイルに出力
+$speak.SetOutputToWaveFile($outputPath)
+$speak.Speak($text)
+$speak.Dispose()
