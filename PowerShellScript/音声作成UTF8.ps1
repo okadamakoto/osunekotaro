369ab17af828de63d7d3a881996022418cd2974d
okadamakoto <osunekotaro.sippo.in.wind@gmail.com>
2025-10-21 17:35:37 +0900

    Add files via upload

---
+++ PowerShellScript/音声作成UTF8.ps1
@@ -0,0 +1,20 @@
+﻿Add-Type -AssemblyName System.Speech
+
+$speak = New-Object System.Speech.Synthesis.SpeechSynthesizer
+
+# 音声（例：日本語音声を指定。必要なら省略可）
+$speak.SelectVoice("Microsoft Haruka Desktop")
+
+# 読み上げ速度をゆっくりに設定
+$speak.Rate = -2
+
+$speak.Volume = 50
+
+# 出力先をWAVファイルに設定（フルパスOK）
+$speak.SetOutputToWaveFile("C:\okada\text\基本情報技術者試験UTF8.wav")
+
+# テキストを読み込んで読み上げ（フルパスOK）
+$speak.Speak([IO.File]::ReadAllText("C:\okada\text\基本情報技術者試験UTF8.txt"))
+
+# 後片付け
+$speak.Dispose()
